cf8ceacb077e66f221346d3736ad2d9eb2c7bc1d
ishr <ishratsingh00@gmail.com>
2020-09-08 23:00:19 -0400

    add plotting notebook

--- benchmark.py
+++ benchmark.py
@@ -1,6 +1,7 @@
 from argparse import ArgumentParser
 import os
 from utils.data import read_lvm
+import pandas as pd
 from models import OnlineMLP
 
 
@@ -9,25 +10,44 @@ parser.add_argument('--history-length', type=int, default=10)
 parser.add_argument('--forecast-length', type=int, default=5)
 parser.add_argument('--hidden-layers', type=int, default=[10], nargs='*')
 parser.add_argument('--epochs-per-sample', type=int, default=1)
+parser.add_argument('--save', action='store_true')
 args = parser.parse_args()
 
-dataset_names = [f for f in os.listdir('data') if not f.endswith('.pkl')]
-print(dataset_names)
+#dataset_names = [f for f in os.listdir('data') if not f.endswith('.pkl')]
+dataset_names = ['Ivol_Acc_Load_data3_w3_w2_50per_STD_downBy256.lvm']
 for d in dataset_names:
     dataset = read_lvm(os.path.abspath(f'data/{d}'))
-
     sample_rate = round(dataset.shape[0] / dataset.iloc[-1]['Time'], 2)
+    results = pd.DataFrame(columns=[
+        'Timestep',
+        'Time',
+        'Observed',
+        'Pred Timestep',
+        'Pred Time',
+        'Predicted'
+    ])
 
-    model = OnlineMLP(**vars(args))
-
-    for _, time, sample in dataset.itertuples():
+    model = OnlineMLP(args.history_length, args.forecast_length, args.hidden_layers, args.epochs_per_sample)
+    for i, time, sample in dataset.query('Time <= 19.7').itertuples():
         pred_time = time + (args.forecast_length / sample_rate)
         pred = model.update(sample)
         print(
-            f'{", ".join(str(x) for x in vars(args).values())}, ',  # model configuration
-            f'{round(sample_rate, 2)}, ',                           # sample rate of dataset
-            f'{round(time, 6)}, ',                                  # current time
-            f'{sample}, ',                                          # current sample
-            f'{round(pred_time, 6)}, ',                             # time being predicted at
-            pred                                                    # model prediction
+            f'{", ".join(str(x) for x in vars(args).values() if x not in [True, False])}, ',  # model configuration
+            f'{round(sample_rate, 2)}, ',                                                     # sample rate of dataset
+            f'{i}, ',                                                                         # current timestep
+            f'{round(time, 6)}, ',                                                            # current time
+            f'{sample}, ',                                                                    # current sample
+            f'{i + args.forecast_length}, ',                                                  # timestep being predicted at
+            f'{round(pred_time, 6)}, ',                                                       # time being predicted at
+            pred                                                                              # model prediction
         )
+
+        results.loc[results.size + 1] = [i, time, sample, i + args.forecast_length, pred_time, pred]
+
+    results = results[['Timestep', 'Time', 'Observed']].merge(
+        results[['Pred Timestep', 'Pred Time', 'Predicted']].rename(columns={'Pred Timestep': 'Timestep'}),
+        how='outer'
+    )
+    results['Time'] = results['Time'].fillna(results['Pred Time'])
+    results = results[['Time', 'Observed', 'Predicted']]
+    results.to_csv(f'results_{d}.csv')
--- utils/metrics.py
+++ utils/metrics.py
@@ -10,27 +10,32 @@ class MetricsAdapter:
         self._observed = self._df.dropna()['Observed'].values
         self._predicted = self._df.dropna()['Predicted'].values
 
+    def __getattr__(self, item):
+        return self._df.item
+
     def overall_mae(self):
         return mean_absolute_error(self._observed, self._predicted)
 
     def overall_rmse(self):
         return np.sqrt(mean_squared_error(self._observed, self._predicted))
 
-    def rolling_mae(self, window_size):
-        return self._rolling_error(window_size, strategy=mean_absolute_error)
+    def rolling_mae(self, window_size, label):
+        return self._rolling_error(window_size, label, strategy=mean_absolute_error)
 
-    def rolling_rmse(self, window_size):
-        return self._rolling_error(window_size, strategy=lambda x, y: np.sqrt(mean_squared_error(x, y)))
+    def rolling_rmse(self, window_size, label):
+        return self._rolling_error(window_size, label, strategy=lambda x, y: np.sqrt(mean_squared_error(x, y)))
 
     def trac(self):
         numerator = np.dot(self._observed, self._predicted) ** 2
         denominator = np.dot(self._observed, self._observed) * np.dot(self._predicted, self._predicted)
         return numerator / denominator
 
-    def _rolling_error(self, window_size, strategy=mean_absolute_error):
+    def _rolling_error(self, window_size, label, strategy=mean_absolute_error):
+        assert window_size >= 1
+
         rolled = np.array([])
-        for i in range(self._df.size):
-            if i <= window_size or np.isnan(self._predicted[i - window_size]):
+        for i in range(self._df.dropna().shape[0]):
+            if i <= window_size:
                 rolled = np.append(rolled, np.nan)
             else:
                 rolled = np.append(
@@ -38,4 +43,4 @@ class MetricsAdapter:
                     strategy(self._observed[(i - window_size):i], self._predicted[(i - window_size):i])
                 )
 
-        return pd.Series(rolled, index=self._df.index)
+        return pd.DataFrame({'Time': self._df.dropna()['Time'].values, label: rolled})
